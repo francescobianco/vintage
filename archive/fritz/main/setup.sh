17d213706b9754c4f6cd394d119e1a9247d9b348
Francesco Bianco <info.francescobianco@gmail.com>
2021-12-03 21:04:08 +0100

    First commit

--- archive/fritz/main/setup.sh
+++ archive/fritz/main/setup.sh
@@ -4,3 +4,4 @@ vintage --task download fritz http://www.retrogames.cz/dos/fritz2.zip
 vintage --task extract fritz main
 vintage --task mount fritz main disk "fritz2.img" --image-type vfat --image-offset 4096
 vintage --task copy fritz main disk FRITZ
+vintage --task entrypoint FRITZ "fritz.exe"
