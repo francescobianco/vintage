107b0e28eff8e97ba2104e49ac9afcb621511793
Francesco Bianco <info.francescobianco@gmail.com>
2021-12-03 22:28:28 +0100

    First commit

---
+++ archive/dosshell/main/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+vintage --task download dosshell https://archive.org/download/msdos_dosshell/dosshell.zip
+vintage --task extract dosshell main
+vintage --task copy dosshell main DOSSHELL
+vintage --task entrypoint DOSSHELL "dosshell.exe"
+vintage --task alias dosshell DOSSHELL
