5bb42cefaa2483a0c1f572d2b3d81a58710825e2
Francesco Bianco <info.francescobianco@gmail.com>
2021-12-04 12:50:45 +0100

    First commit

--- contrib/demo.sh
+++ contrib/demo.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#ghostplay silent
+ghostplay_custom_prompt() { # change prompt
+  printf '$ '
+}
+#ghostplay end
+
 ## Install "vintage" software manager
 curl -s binst.tk | sudo bash -s francescobianco/vintage
 
