e5e5f6f6209202971fab6251a0aad450fb4c9960
Francesco Bianco <info.francescobianco@gmail.com>
2021-12-05 11:45:37 +0100

    First commit

--- archive/qbasic/main/setup.sh
+++ archive/qbasic/main/setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-vintage --task download qbasic https://winworldpc.com/download/c5a11f1d-c386-c592-7311-c3a5c28f1352/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
-vintage --task extract qbasic main --file-format 7z
-vintage --task mount qbasic/main disk01 "Microsoft DOS QBasic 1.1/disk01.img"
-vintage --task copy qbasic/main/disk01 QBASIC
+## WinWorld Download
+#vintage --task download qbasic https://winworldpc.com/download/c5a11f1d-c386-c592-7311-c3a5c28f1352/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
+#vintage --task extract qbasic main --file-format 7z
+#vintage --task mount qbasic/main disk01 "Microsoft DOS QBasic 1.1/disk01.img"
+#vintage --task copy qbasic/main/disk01 QBASIC
+#vintage --task shim QBASIC "qbasic.exe"
+#vintage --task alias qbasic QBASIC
+
+## Alternative Download
+vintage --task download qbasic https://hwiegman.home.xs4all.nl/downloads/qbasic11.zip
+vintage --task extract qbasic main
+vintage --task copy qbasic/main QBASIC
 vintage --task shim QBASIC "qbasic.exe"
 vintage --task alias qbasic QBASIC
