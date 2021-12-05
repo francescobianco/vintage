cfef5a0bf5d01da4982c27e1ca97204cd38ba5d6
Francesco Bianco <info.francescobianco@gmail.com>
2021-12-05 10:54:07 +0100

    First commit

--- archive/qbasic/main/setup.sh
+++ archive/qbasic/main/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-vintage --task download qbasic https://winworldpc.com/download/e280b0c3-8602-c392-c592-7311c3a5c28f/from/c39ac2af-c381-c2bf-1b25-11c3a4e284a2
-vintage --task extract qbasic main --file-format 7z
-vintage --task mount qbasic/main disk01 "Microsoft DOS QBasic 1.0/disk01.img"
-vintage --task copy qbasic/main/disk01 QBASIC
+vintage --task download qbasic https://archive.org/download/GorillasQbasic/gorillas_qbasic.zip
+#vintage --task extract qbasic main --file-format 7z
+vintage --task extract qbasic main
+#vintage --task mount qbasic/main disk01 "Microsoft DOS QBasic 1.0/disk01.img"
+#vintage --task copy qbasic/main/disk01 QBASIC
+vintage --task copy qbasic/main QBASIC
 vintage --task shim QBASIC "qbasic.exe"
 vintage --task alias qbasic QBASIC
