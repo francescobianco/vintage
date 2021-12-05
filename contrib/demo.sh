1d8067998d0061a811ecbfad7e5a35c11c1fe481
Francesco Bianco <info.francescobianco@gmail.com>
2021-12-05 12:17:03 +0100

    First commit

--- contrib/demo.sh
+++ contrib/demo.sh
@@ -4,26 +4,40 @@
 ghostplay_custom_prompt() { # change prompt
   printf '$ '
 }
+ghostplay_before_prompt_hook() {
+  :
+}
+ghostplay_after_prompt_hook() {
+  ghostplay_sleep 1
+}
 #ghostplay end
 
-## Install "vintage" software manager
-##
+# Install "Vintage" Software Manager
+#
 curl -s binst.tk | sudo bash -s francescobianco/vintage
 #ghostplay silent
 sleep 3
 #ghostplay end
 
-##
-## Add your nostalgic software
-##
+#
+# Add Your Nostalgic Software
+#
+#ghostplay silent
+sleep 1
+#ghostplay end
 vintage add qbasic
 #ghostplay silent
 sleep 3
 #ghostplay end
 
-##
-## Run the MAGIC!
-##
+#
+# Run the MAGIC!
+#
+# just type:  vintage run qbasic
+#
+#ghostplay silent
+sleep 1
+#ghostplay end
 vintage run -t qbasic
 #ghostplay silent
 sleep 3
